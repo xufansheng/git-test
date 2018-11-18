1e283e7c6e96cc0869b3a919ad08f2907282b0ac
zhangsan <zhangsan@itcast.cn>
2018-11-18 21:31:54 +0800

    把本地工程添加进来

---
+++ java-project/java-test-project/src/com/ithenma/git/FirstClass.java
@@ -0,0 +1,9 @@
+package com.ithenma.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
